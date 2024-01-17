f9df8de0aaf691ebff3c73c3c96c4d7003652d2e
KrystianRusin <krystian.r@hotmail.com>
2024-01-17 14:07:13 -0500

    added carousel on homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/category/men's clothing?limit=5")
+      .then((res) => res.json())
+      .then((json) => setProducts(json));
+  }, []);
+
+  return (
+    <div className="flex justify-center">
+      <div className="max-w-[450px] max-h-[300px]">
+        <Carousel className="h-full">
+          {products.map((product) => (
+            <div
+              key={product.id}
+              className="h-full flex items-center justify-center"
+            >
+              <img
+                src={product.image}
+                alt={product.title}
+                className="max-h-full object-contain"
+              />
+              <p className="legend">{product.title}</p>
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,16 @@ import "../styles/Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-between py-5 px-20 bg-white w-full">
+    <div className="flex items-center justify-between py-5 px-20 bg-white w-full border-b border-slate-200">
       <div className="text-slate-950">Logo</div>
-      <div className="flex-grow max-w-[750px]">
+      <div className="flex-grow max-w-[900px] flex just">
         <input
-          className="px-3 py-2 mr-4 rounded-md border w-full border-slate-950 focus:outline-none focus:ring-2 focus:ring-slate-950 focus:border-transparent"
+          className="px-3 py-2 rounded-md border w-full border-slate-950 focus:outline-none focus:ring-2 focus:ring-slate-950 focus:border-transparent"
           type="text"
           placeholder="Search..."
         />
       </div>
-      <div className="flex">
+      <div className="flex gap-5">
         <Link to="/" className="px-3 py-2 mr-2 bg-white nav-item">
           Home
         </Link>
